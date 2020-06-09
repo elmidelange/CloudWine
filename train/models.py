e5d1e5b90da0bce4bf8afa2d519c36c6974301f1
Elmi de Lange <elmidelange@gmail.com>
2020-06-09 16:10:58 +1000

    Added Doc2Vec model to train_model.py

--- train/models.py
+++ train/models.py
@@ -1,6 +1,9 @@
 import pickle
+import numpy as np
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
 
 from utils import logger
 
@@ -30,4 +33,55 @@ class TfidfTrainer:
         # Save corpus text vectors
         logger.info("Saving vectors to %s", path)
         with open(path +  '/tfidf_matrix.pkl', "wb") as pickleFile:
+            pickle.dump(self.text_vectors, pickleFile)
+
+
+# Doc2Vec model training class
+class DocVecTrainer:
+    def __init__(self, args):
+        self.model = None
+        self.text_vectors = None
+        self.vec_size = args['vec_size']
+        self.max_epochs = args['max_epochs']
+
+    # Train Doc2Vec model
+    def train(self, corpus):
+        logger.info("Training model")
+        tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(corpus)]
+        alpha = 0.025
+        # Create model
+        model = Doc2Vec(size=self.vec_size,
+                        alpha=alpha,
+                        min_alpha=0.00025,
+                        min_count=1,
+                        dm =1)
+        model.build_vocab(tagged_data)
+        # Train model
+        for epoch in range(self.max_epochs):
+            logger.info('iteration {0}'.format(epoch))
+            model.train(tagged_data,
+                        total_examples=model.corpus_count,
+                        epochs=model.iter)
+            # decrease the learning rate
+            model.alpha -= 0.0002
+            # fix the learning rate, no decay
+            model.min_alpha = model.alpha
+        self.model = model
+
+    # Returns the trained sentence vectors
+    def get_vectors(self):
+        docvecs = self.model.docvecs['0']
+        for i in range(1,self.model.corpus_count):
+            docvecs = np.vstack((docvecs, self.model.docvecs[str(i)]))
+        return docvecs
+
+    # Save the model in pickle format
+    def save(self, path):
+        logger.info("Saving model to %s", path)
+        # Save TfidfVectoriser vocab
+        with open(path + '/doc2vec_model.pkl', "wb") as pickleFile:
+            pickle.dump(self.model, pickleFile)
+        # Save corpus text vectors
+        logger.info("Saving vectors to %s", path)
+        with open(path +  '/doc2vec_vectors.pkl', "wb") as pickleFile:
             pickle.dump(self.text_vectors, pickleFile)
--- train/train_model.py
+++ train/train_model.py
@@ -1,31 +1,40 @@
 # Imports
 import os
 import argparse
+import yaml
 
-from utils import Dataset, Validation
-from models import TfidfTrainer
+from utils import Dataset, Validation, logger
+from models import TfidfTrainer, DocVecTrainer
 
 
 # Main function
-def main(args):
+def main(config):
+
+    # Initialise the model type and arguments
+    model, args = init_trainer(config)
+
+    print(args)
+
     # Read training data
     dataset = Dataset(args['data_path'])
     corpus = dataset.get_corpus()
 
     # Train TF-IDF model
-    model = TfidfTrainer()
+    # model = TfidfTrainer()
     model.train(corpus)
 
-    # Save run
-    model.save(args['model_dir'])
-    dataset.save(args['model_dir'])
+    if args['save'] == True:
+        # Save run
+        model.save(args['model_dir'])
+        dataset.save(args['model_dir'])
 
-    # Validate
-    valid = Validation()
-    x = model.get_vectors()
-    df = dataset.get_df()
-    valid.plot_pca(x, df['variety_region'])
-    print(valid.cluster_similarities(x, df))
+    if args['validation'] == True:
+        # Validate
+        valid = Validation()
+        x = model.get_vectors()
+        df = dataset.get_df()
+        valid.plot_pca(x, df['variety_region'])
+        print(valid.cluster_similarities(x, df))
 
 
 # Checks for valid directory
@@ -43,30 +52,68 @@ def check_file_exists(path):
     else:
         raise ValueError(path)
 
+# Checks for valid arguments
+def check_config(path):
+    # Check file exists
+    if not (os.path.isfile(path)):
+        ValueError(path)
+    # Read file into json
+    try:
+        with open(path) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+    except Exception as e:
+        logger.info(e)
+        logger.error('Invalid parameters yaml ' + path)
+        raise ValueError(path)
+        exit()
+    # Check for parameters
+    if not all(key in config for key in ('model', 'args')):
+        logger.error("Missing 'model' or 'args' parameter in config yaml " + path)
+        raise ValueError(path)
+        exit()
+    model = config['model']
+    args = config['args']
+    if not all(key in args for key in ('data_path', 'model_dir', 'validation', 'save')):
+        logger.error("Missing 'data_path' or 'model_dir' args in config yaml " + path)
+        raise Exception("Config yaml missing one of 'data_path', 'model_dir', 'validation', 'save'")
+        sys.exit(1)
+    return config
+
+
+# Returns a model trainer and arguments
+def init_trainer(config):
+    model_type = config['model']
+    args = config['args']
+    if model_type == 'tfidf':
+        model = TfidfTrainer()
+    elif model_type == 'doc2vec':
+        model = DocVecTrainer(args)
+        pass
+    elif model_type == 'bert':
+        # TODO: Add transformers model check_dir_exists
+        pass
+    else:
+        raise ValueError(model_type)
+
+    return model, args
+
 
 # Returns argument parser
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s [DATA_PATH] [MODE_DIR]",
+        usage="%(prog)s [CONFIG_YAML]",
         description="Train a TD-IDF model"
     )
     parser.add_argument(
-        "-d", "--data_path", help="Training data file path",
-            default='./data/raw/sample.csv', type=check_file_exists
+        "-y", "--config_yaml", help="Training config yaml file",
+            default='./args.yaml', type=check_config
     )
-    parser.add_argument(
-        "-m", "--model_dir", help="Model directory to save into",
-            default='./model', type=check_dir_exists
-    )
-    # parser.add_argument(
-    #     "-v", "--validation", help="Run validation",
-    #         store=True
-    # )
     return parser
 
 
 if __name__ == "__main__":
     # execute only if run as a script
     parser = init_argparse()
-    args = parser.parse_args()
-    main(vars(args))
+    config_ns = parser.parse_args()
+    config = vars(config_ns)['config_yaml']
+    main(config)
