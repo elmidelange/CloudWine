212c158ab002ee9315967faa896d1acafae8f1bc
Elmi de Lange <elmidelange@gmail.com>
2020-06-16 15:04:28 +1000

    Added nltk library

--- cloudwine/inference.py
+++ cloudwine/inference.py
@@ -5,6 +5,8 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
 
 
 # Loads TF-IDF model and inferences input string
